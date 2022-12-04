6a7c56931e98cfc5e23d68d80e24aed602d373d2
Ismet Marek <116670021+DerIsmet@users.noreply.github.com>
2022-12-04 20:51:53 +0100

    Add files via upload

---
+++ C#/radioactivity_decay/radioactivity_decay/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace radioactivity_decay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radiation = 100;
+            int year = 0;
+
+
+
+            Console.WriteLine("Radioaktiver Zerfall");
+            Console.WriteLine("");
+            do
+            {
+                
+                Console.WriteLine("After " + year + " years there are " + radiation + "% radioactivity still there.");
+
+                radiation = radiation * 0.95;
+                year++;
+            } while (radiation >= 50);
+            Console.WriteLine("");
+            Console.WriteLine("After " + year + " years the raduiacticity decayed to half.");
+            Console.ReadKey();
+        }
+    }
+}
