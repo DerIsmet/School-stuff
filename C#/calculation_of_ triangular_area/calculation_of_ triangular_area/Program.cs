ccf8096e0dd683e3d1e9bdcd14708a3f8340b922
Ismet Marek <116670021+DerIsmet@users.noreply.github.com>
2022-12-04 18:30:00 +0100

    Add files via upload

---
+++ C#/calculation_of_ triangular_area/calculation_of_ triangular_area/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculation_of__triangular_area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double trianglelength;
+            double trianglehigh;
+            double trianglearea;
+
+            Console.WriteLine("Program to calculate the area of a triangular");
+            Console.Write("Please type in the lenght of the basesite from the triangule: ");
+            trianglelength = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Please type in the high of the triangule: ");
+            trianglehigh = Convert.ToDouble(Console.ReadLine());
+            trianglearea = trianglelength * trianglehigh;
+            trianglearea = trianglearea / 2;
+            Console.WriteLine("The area is "+ trianglearea);
+
+            Console.ReadKey();
+
+        }
+    }
+}
