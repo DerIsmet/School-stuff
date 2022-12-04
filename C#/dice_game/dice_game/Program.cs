b9203402357dfc25d65e48e22de9c7c6bc50e462
Ismet Marek <116670021+DerIsmet@users.noreply.github.com>
2022-12-04 20:32:35 +0100

    Add files via upload

---
+++ C#/dice_game/dice_game/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dice_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int dice = 0;
+            int sum = 0;
+                
+            Console.WriteLine("Würfelspiel");
+            Console.WriteLine("===========");
+
+            while (dice != 6)
+            {
+                dice = random.Next(1, 7);
+                Console.WriteLine("diced number: " + dice + "");
+                sum += dice;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Sum: "+sum);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ C#/sum_program/sum_program/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sum_program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int userinputa;
+            int userinputb;
+            int sum;
+            char yorn;
+
+            do
+            {
+                Console.WriteLine("--------------------------------");
+                Console.Write("Please type in the 1 number: ");
+                userinputa = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Please type in the 1 number: ");
+                userinputb = Convert.ToInt32(Console.ReadLine());
+                sum = userinputa + userinputb;
+                Console.WriteLine("The sum is: " + sum + "");
+                Console.WriteLine("--------------------------------");
+                Console.Write("Do you want to do it again? (Y/N) ");
+                yorn = Convert.ToChar(Console.ReadLine());
+                
+
+            } while (yorn == 'Y');
+            Console.ReadKey();
+
+        }
+    }
+}
